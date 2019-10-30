c580ac362bdc8b04bcb64e854656de245a804358
thakurtejveer <thakurtejveer1@gmail.com>
2019-10-30 17:43:31 +0530

    ParamMap Observables done

--- src/app/singer-detail/singer-detail.component.ts
+++ src/app/singer-detail/singer-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
 // import { ActivatedRout } from '@angular/router';
 
 @Component({
@@ -10,18 +10,32 @@ import { ActivatedRoute } from '@angular/router';
 export class SingerDetailComponent implements OnInit {
 
   public employeeId;
-  public employeeName;
-  public employeeAge;
+  // public employeeName;
+  // public employeeAge;
 
-  constructor(private route :ActivatedRoute) { }
+  constructor(private activatedRoute :ActivatedRoute, private route : Router ) { }
 
   ngOnInit() {
-    let Id=parseInt(this.route.snapshot.paramMap.get('id'));
+    // let Id=parseInt(this.activatedRoute.snapshot.paramMap.get('id'));
     // let Age=this.route.snapshot.paramMap.get('age');
     // let Name=this.route.snapshot.paramMap.get('name');
-    this.employeeId=Id;
+    // this.employeeId=Id;
     // this.employeeName=Name;
     // this.employeeAge=Age;
+    this.activatedRoute.paramMap.subscribe((params:ParamMap)=> {
+      let Id=parseInt(params.get('id'));
+      this.employeeId=Id;
+    });
+  }
+
+  goPrevious() {
+    let previousId=this.employeeId-1;
+    this.route.navigate(['/employeeList',previousId]);
+  }
+  goNext() {
+    let nextId=this.employeeId+1;
+    this.route.navigate(['/employeeList',nextId]);
+
   }
 
 }
